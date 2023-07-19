b4e7ce02de5b0a8a1fa6e00bc4211759b23e3de9
whoshe <whoshe@gmail.com>
2023-07-19 18:17:54 +0900

    trivial  updated

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container">
+    <main className="container px-4">
       <div className="navbar bg-base-100">
         <a className="btn-lg btn-ghost normal-case text-xl">daisyUI</a>
       </div>
